55cb2b62945eb30e2c4212e12e140f3fd96eea0a
YC Xu <x13989@126.com>
2018-01-11 21:07:28 +0800

    添加项目文件。

---
+++ AlgebraicLib/Letter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraicLib
+{
+    class Letter
+    {
+        public char Value;
+        public Letter(char vaule)
+        {
+            Value = vaule;
+        }
+    }
+}
---
+++ CPP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgebraicLib;
+
+namespace CPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression e = new Expression(Console.ReadLine());
+            e.OutputElement();
+            Console.WriteLine(e.Length);
+            Console.ReadKey();
+        }
+    }
+}
