ce39119b4202927545d10d36d0d80effbee1f26c
CSharp AHS <33123710+HydrogenC@users.noreply.github.com>
2018-01-15 13:57:54 +0800

    Update Fraction.Calculate.cs

--- Fraction/Fraction.Calculate.cs
+++ Fraction/Fraction.Calculate.cs
@@ -32,39 +32,11 @@ namespace Fraction
             return new Fraction(BigInteger.Parse(a.ToString()), BigInteger.Parse(b.ToString()));
         }
 
-        public static Tuple<BigInteger, BigInteger> Reduction(BigInteger numerator,BigInteger denominator)
+        private void Reduct()
         {
             BigInteger gcd = GCD.GCD.GetGCD(numerator, denominator).GCD;
             numerator = numerator / gcd;
             denominator = denominator / gcd;
-            return new Tuple<BigInteger, BigInteger>(numerator,denominator);
-        }
-
-        public static Fraction operator *(Fraction a, Fraction b)
-        {
-            return new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
-        }
-
-        public static Fraction operator +(Fraction a, Fraction b)
-        {
-            BigInteger lcm = GCD.GCD.GetGCD(a.Denominator, b.Denominator).LCM;
-            return new Fraction(a.Numerator * (lcm / a.Denominator) + b.Numerator * (lcm / b.Denominator), lcm);
-        }
-
-        public static Fraction operator -(Fraction a, Fraction b)
-        {
-            BigInteger lcm = GCD.GCD.GetGCD(a.Denominator, b.Denominator).LCM;
-            return new Fraction(a.Numerator * (lcm / a.Denominator) - b.Numerator * (lcm / b.Denominator), lcm);
-        }
-
-        public static Fraction operator /(Fraction a, Fraction b)
-        {
-            return a * b.Reciprocal();
-        }
-
-        public static Fraction operator ^(Fraction a, Decimal b)
-        {
-            return a.Pow(b);
         }
     }
 }
