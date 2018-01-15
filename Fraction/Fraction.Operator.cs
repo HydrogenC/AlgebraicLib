faf88cf7aecae4a2573d8b99b1893f44c9c99e85
CSharp AHS <33123710+HydrogenC@users.noreply.github.com>
2018-01-15 14:01:21 +0800

    Create Fraction.Operator.cs

---
+++ Fraction/Fraction.Operator.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Numerics;
+
+namespace Fraction{
+    public paritial class Fraction{
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            BigInteger lcm = GCD.GCD.GetGCD(a.Denominator, b.Denominator).LCM;
+            return new Fraction(a.Numerator * (lcm / a.Denominator) + b.Numerator * (lcm / b.Denominator), lcm);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            BigInteger lcm = GCD.GCD.GetGCD(a.Denominator, b.Denominator).LCM;
+            return new Fraction(a.Numerator * (lcm / a.Denominator) - b.Numerator * (lcm / b.Denominator), lcm);
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return a * b.Reciprocal();
+        }
+        
+        public static Boolean operator ==(Fraction a, Fraction b)
+        {
+            
+        }
+    }
+}
